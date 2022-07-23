998f2d2a74b4a239d39de4067e2171ddd618862f
Alexey Chub <33015958+alexchubgit@users.noreply.github.com>
2022-07-23 10:41:12 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	fmt.Println("Hello, Modules!")
 }
